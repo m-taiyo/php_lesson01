a66e691faded1c116fe563500d73f21a91d5b632
taiyo maruyama <85174720+m-taiyo@users.noreply.github.com>
2022-04-28 11:11:21 +0900

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Docker Test</title>
+</head>
+<body>
+  <form action="index.php" method='post'>
+    <input type="text" name = 'test' >
+    <input type="submit" name = '送信'>
+  </form>
+
+  <?php
+  ?>
+
+
+
+</body>
+</html>
